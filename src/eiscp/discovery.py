be90799a5f287cf3f85d79c88076e520d5ab4844
Christoph Brand <christoph@xcnt.io>
2021-01-02 22:15:02 +0100

    feature(discovery): socket lock for discovery initialization

--- src/eiscp/discovery.py
+++ src/eiscp/discovery.py
@@ -35,6 +35,9 @@ def get_current_broadcast_address() -> str:
     return ".".join(str(item) for item in broadcast_segments)
 
 
+discovery_lock = uasyncio.Lock()
+
+
 async def discover(timeout: int = 5, clazz=None):
     onkyo_magic = eISCPPacket("!xECNQSTN").get_raw()
     pioneer_magic = eISCPPacket("!pECNQSTN").get_raw()
@@ -44,27 +47,28 @@ async def discover(timeout: int = 5, clazz=None):
     broadcast_address = get_current_broadcast_address()
     own_address = get_current_ip_address()
 
-    try:
-        broadcast_socket.bind(
-            (own_address, eISCP.ONKYO_PORT)
-        )  # The port doesn't matter. It is "0" in the original implementation. MicroPython doesn't support this.
-        broadcast_socket.sendto(onkyo_magic, (broadcast_address, eISCP.ONKYO_PORT))
-        broadcast_socket.sendto(pioneer_magic, (broadcast_address, eISCP.ONKYO_PORT))
-        start = time.ticks_ms()
-        while True:
-            ready = uasyncio.select.select([broadcast_socket], [], [], 0.01)
-            if not ready[0]:
-                await uasyncio.sleep_ms(100)
-            else:
-                data, addr = broadcast_socket.recvfrom(1024)
-                info = parse_info(data)
-                receiver = (clazz or eISCP)(addr[0], int(info["iscp_port"]))
-                receiver.info = info
-                found_receivers[info["identifier"]] = receiver
+    async with discovery_lock:
+        try:
+            broadcast_socket.bind(
+                (own_address, eISCP.ONKYO_PORT)
+            )  # The port doesn't matter. It is "0" in the original implementation. MicroPython doesn't support this.
+            broadcast_socket.sendto(onkyo_magic, (broadcast_address, eISCP.ONKYO_PORT))
+            broadcast_socket.sendto(pioneer_magic, (broadcast_address, eISCP.ONKYO_PORT))
+            start = time.ticks_ms()
+            while True:
+                ready = uasyncio.select.select([broadcast_socket], [], [], 0.01)
+                if not ready[0]:
+                    await uasyncio.sleep_ms(100)
+                else:
+                    data, addr = broadcast_socket.recvfrom(1024)
+                    info = parse_info(data)
+                    receiver = (clazz or eISCP)(addr[0], int(info["iscp_port"]))
+                    receiver.info = info
+                    found_receivers[info["identifier"]] = receiver
 
-            if start + timeout * 1000 < time.ticks_ms():
-                break
+                if start + timeout * 1000 < time.ticks_ms():
+                    break
 
-    finally:
-        broadcast_socket.close()
+        finally:
+            broadcast_socket.close()
     return list(found_receivers.values())
